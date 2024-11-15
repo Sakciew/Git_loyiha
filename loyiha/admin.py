355cfbe93be5afaa8f90434c082df19285a8064a
Sakciew <shijaganiyev@gmail.com>
2024-11-15 19:08:27 +0500

    Yuklash

---
+++ loyiha/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Category, Region, New
+
+admin.site.register(New)
+admin.site.register(Category)
+admin.site.register(Region)
+
+
+
+
---
+++ loyiha/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+    objects = None
+    name=models.CharField(max_length=100)
+    image = models.ImageField(upload_to='posts/', null=True)
+
+    def __str__(self):
+        return self.name
+
+class Region(models.Model):
+    objects = None
+    name=models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
+class New(models.Model):
+    objects = None
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, blank=True, null=True)
+    title = models.CharField(max_length=300)
+    text = models.TextField()
+    image = models.ImageField(upload_to='posts/')
+    date = models.DateField(auto_now=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("detail", args=[str(self.pk)])
+
+
+    
+    
